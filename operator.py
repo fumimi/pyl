5e35dc290ad26282f275db4f591217df24cd9fcd
fumimi <info@fumimi.jp>
2016-12-18 10:43:04 +0900

    演算子

---
+++ operator.py
@@ -0,0 +1,22 @@
+# 演算子
+# + - * / // % **
+
+x = 10
+print(x / 3) # 3.33... （商）
+print(x // 3) # 3 （）
+print(x % 3) # 1 （余り）
+print(x ** 2) # 100 （二乗）
+
+y = 4
+# y = y + 12
+y += 12
+print(y) # 16
+
+# and or not
+print(True and False) # False
+print(True or False) # True
+print(not True) # False
+
+# + *
+print("hello " + "world")
+print("hello " * 3)

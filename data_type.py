6f5dc13466e7dbfd9cef9aabdb2c005791d2d98a
fumimi <info@fumimi.jp>
2016-12-18 10:42:44 +0900

    データ型

---
+++ data_type.py
@@ -0,0 +1,19 @@
+# 文字列
+s = "he\nllo wor\tld"
+html = """<html>
+<body></body>
+</html>"""
+print(s)
+print(html)
+
+# 整数
+i = 10
+print(i)
+
+# 浮動小数点
+f = 23.4
+print(f)
+
+# 論理値
+flag = True # False
+print(flag)
